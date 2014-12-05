c8837bfca40496aa77a5872d45b18a1d6ec70183
Bernard Nongpoh <bernard.nongpoh@gmail.com>
2014-12-05 22:41:38 +0530

    move to models packages

---
+++ src/org/smcs/model/Coordinate.java
@@ -0,0 +1,36 @@
+package org.smcs.model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author cs12shea8
+ */
+public class Coordinate {
+    private double longitude;
+    private double latitude;
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+    
+    
+}
